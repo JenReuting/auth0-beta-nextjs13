f8b2aca68f290ba97a5b395b2eff56d82f375aef
Jennifer Reuting <jenreuting@yahoo.com>
2023-07-07 16:39:29 -0700

    added auth0 middleware and applied to profile api route and page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,28 @@
 'use client';
 
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
-const inter = Inter({ subsets: ['latin'] })
-
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+	const { user, error, isLoading } = useUser();
 
-  const { user, error, isLoading } = useUser();
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>{error.message}</div>;
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+	if (user) {
+		return (
+			<div>
+				Welcome {user.name}! <a href='/api/auth/logout'>Logout</a>
+			</div>
+		);
+	}
 
-  if (user) {
-    return (
-      <div>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <a href="/api/auth/login">Login</a>;
-    </div>
-  )
+	return (
+		<div>
+			<a href='/api/auth/login'>Login</a>;
+		</div>
+	);
 }
