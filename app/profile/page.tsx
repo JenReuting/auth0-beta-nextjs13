0410430602edece9c56e32f356c2ba0e8043f356
Jennifer Reuting <jenreuting@yahoo.com>
2023-07-10 12:51:33 -0700

    added example env file

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,23 +4,37 @@ import ServerComponent from '@/app/server-component';
 import ClientComponent from '@/app/client-component';
 
 export default withPageAuthRequired(
-  async function Page() {
-    const session = await getSession();
+	async function Page() {
+		const session = await getSession();
 
-    return (
-      <main>
-        <h1>Profile</h1>
-        <h2>Page:</h2>
-        <h3>Access Token</h3>
-        <pre>{JSON.stringify({ accessToken: session?.accessToken }, null, 2)}</pre>
-        <h3>User</h3>
-        <pre>{JSON.stringify(session?.user, null, 2)}</pre>
-        <h2>Server Component:</h2>
-        <ServerComponent />
-        <h2>Client Component:</h2>
-        <ClientComponent />
-      </main>
-    );
-  },
-  { returnTo: '/profile' }
+		return (
+			<main>
+				<h1>Profile</h1>
+				<h2>Page:</h2>
+				<h3>Access Token</h3>
+				<pre>
+					{JSON.stringify(
+						{ accessToken: session?.accessToken },
+						null,
+						2
+					)}
+				</pre>
+				<h3>Refresh Token</h3>
+				<pre>
+					{JSON.stringify(
+						{ refreshToken: session?.refreshToken },
+						null,
+						2
+					)}
+				</pre>
+				<h3>User</h3>
+				<pre>{JSON.stringify(session?.user, null, 2)}</pre>
+				<h2>Server Component:</h2>
+				<ServerComponent />
+				<h2>Client Component:</h2>
+				<ClientComponent />
+			</main>
+		);
+	},
+	{ returnTo: '/profile' }
 );
